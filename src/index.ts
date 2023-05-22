ba9ee3e60fa37ac55d4ecdf6cd014360b2b30cc9
Jean Demel <jean@demel.com>
2023-05-22 16:37:21 +0200

    parcel config

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+let maVariable = 'une valeur';
+
+
+if(maVariable.length > 3) {
+    console.log(maVariable);
+} else {
+    console.log('condition fausse');
+}
+
+for(let i = 0; i < 10; i++) {
+    console.log(maVariable, i);
+
+}
