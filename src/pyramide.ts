a560e7a0c8233aefb245b6b3e8d6ceff84da7bd0
Jean Demel <jean@demel.com>
2023-05-22 16:59:19 +0200

    added pyramid stars

---
+++ src/pyramide.ts
@@ -0,0 +1,28 @@
+let nbEtoile = '*';
+
+for (let i = 0; i < 5; i++) {    
+    
+    console.log(nbEtoile);
+    nbEtoile+='**';
+    
+}
+
+
+
+//version avec boucles imbriquées
+/*
+let nbEtoile = 3;
+
+for (let i = 0; i < 5; i++) {    
+    
+    let etoiles = '';
+    for (let j = 0; j < nbEtoile; j++) {
+        etoiles += '*';
+    }
+
+    console.log(etoiles);
+    nbEtoile+=2;
+    
+}
+
+*/
